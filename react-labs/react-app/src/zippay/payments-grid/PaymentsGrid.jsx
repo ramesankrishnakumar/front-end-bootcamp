1c4c5c8a4599fc9aa72b697dd58e4d1e4a7f3009
KK Ramesan <krishnakumar_ramesan@intuit.com>
2024-04-23 16:28:09 -0700

    Day-7 Payments-Grid col selector

--- react-labs/react-app/src/zippay/payments-grid/PaymentsGrid.jsx
+++ react-labs/react-app/src/zippay/payments-grid/PaymentsGrid.jsx
@@ -5,10 +5,18 @@ import PaymentsGridBody from './PaymentsGridBody';
 import './PaymentsGrid.css';
 
 function PaymentsGrid() {
+	let columnMap = [
+		{ label: 'ID', field: 'id', visible: false },
+		{ label: 'Sender', field: 'payorId', visible: true },
+		{ label: 'Recipient', field: 'payeeId', visible: true },
+		{ label: 'Date', field: 'datePaid', visible: false },
+		{ label: 'Amount', field: 'amount', visible: true },
+		{ label: 'Reason', field: 'reason', visible: true },
+	];
 	return (
 		<div className="pgContainer">
-			<PaymentsGridHeader />
-			<PaymentsGridBody payments={payments} />
+			<PaymentsGridHeader columnMap={columnMap} />
+			{/* <PaymentsGridBody payments={payments} /> */}
 		</div>
 	);
 }
