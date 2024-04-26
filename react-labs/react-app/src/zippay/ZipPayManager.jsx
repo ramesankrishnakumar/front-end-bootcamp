158c54ac943ffd2b29206c9ef6c37c1cae875f17
John Paxton <johnpaxton@gmail.com>
2024-04-26 16:57:33 -0400

    Added Redux, not yet attached to components

--- react-labs/react-app/src/zippay/ZipPayManager.jsx
+++ react-labs/react-app/src/zippay/ZipPayManager.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import Navbar from './Navbar';
 import './zippay.css';
 import PaymentsGrid from './payments-grid/PaymentsGrid';
-import { Route, Routes } from 'react-router-dom';
 import SendReceive from './send-receive/SendReceive';
 import SplitBill from './split-bill/SplitBill';
 import ZipPaySearch from './search/ZipPaySearch';
@@ -10,36 +12,38 @@ import AccountSettings from './settings/AccountSettings';
 import UserDetailsLookUp from './user-details/UserDetailsLookUp';
 function ZipPayManager() {
 	return (
-		<section className="zippay-main">
-			<Navbar></Navbar>
+		<Provider store={store}>
+			<section className="zippay-main">
+				<Navbar></Navbar>
 
-			<Routes>
-				<Route
-					path="send-receive"
-					element={<SendReceive />}
-				/>
-				<Route
-					path="split-the-bill"
-					element={<SplitBill />}
-				/>
-				<Route
-					path="search"
-					element={<ZipPaySearch />}
-				/>
-				<Route
-					path="payments-grid"
-					element={<PaymentsGrid />}
-				/>
-				<Route
-					path="settings"
-					element={<AccountSettings />}
-				/>
-				<Route
-					path="user-details/:userId"
-					element={<UserDetailsLookUp />}
-				/>
-			</Routes>
-		</section>
+				<Routes>
+					<Route
+						path="send-receive"
+						element={<SendReceive />}
+					/>
+					<Route
+						path="split-the-bill"
+						element={<SplitBill />}
+					/>
+					<Route
+						path="search"
+						element={<ZipPaySearch />}
+					/>
+					<Route
+						path="payments-grid"
+						element={<PaymentsGrid />}
+					/>
+					<Route
+						path="settings"
+						element={<AccountSettings />}
+					/>
+					<Route
+						path="user-details/:userId"
+						element={<UserDetailsLookUp />}
+					/>
+				</Routes>
+			</section>
+		</Provider>
 	);
 }
 
